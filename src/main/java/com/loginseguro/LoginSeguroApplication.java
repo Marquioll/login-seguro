d00934aece8e1ddb37b6d1baaa03ac4c2bfbc7e3
Marquioll <lucasmhorta@hotmail.com>
2026-09-22 17:01:08 -0300

    chore: estrutura inicial do projeto de login

---
+++ src/main/java/com/loginseguro/LoginSeguroApplication.java
@@ -0,0 +1,13 @@
+package com.loginseguro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSeguroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSeguroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loginseguro/LoginSeguroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginseguro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSeguroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
